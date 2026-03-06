e990e27d421ace57396a1035811476cd09e19df0
NathanFilipowitz <py58wpb@eduvaud.ch>
2026-03-06 22:11:38 +0100

    (feat) added database creation from MLD

--- src/models/db.ts
+++ src/models/db.ts
@@ -3,14 +3,42 @@ import { Database } from "bun:sqlite";
 export const db = new Database("shop.sqlite");
 
 export function setupDatabase() {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS products (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      price REAL NOT NULL,
-      stock INTEGER NOT NULL DEFAULT 0
-    )
-  `);
+  db.run(`PRAGMA foreign_keys = ON;`);
   
-  console.log("Database ready");
+  db.transaction(() => {
+    db.run(`
+      CREATE TABLE IF NOT EXISTS clients (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      )
+    `);
+
+    db.run(`
+      CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        price REAL NOT NULL,
+        stock INTEGER DEFAULT 0
+      )
+    `);
+
+    db.run(`
+      CREATE TABLE IF NOT EXISTS commands (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        status TEXT NOT NULL,
+        address TEXT NOT NULL,
+        client_id INTEGER NOT NULL
+      )
+    `);
+
+    db.run(`
+      CREATE TABLE IF NOT EXISTS order_items (
+        product_id INTEGER NOT NULL,
+        command_id INTEGER NOT NULL,
+        quantity INTEGER DEFAULT 1
+      )
+    `);
+  })();
 }
