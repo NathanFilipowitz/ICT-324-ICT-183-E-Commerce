5bdab74382d90774a2cbf31768dec06221912122
NathanFilipowitz <py58wpb@eduvaud.ch>
2026-03-13 15:27:55 +0100

    (docs) changed file purpose

--- src/controllers/shop.controller.ts
+++ src/controllers/shop.controller.ts
@@ -2,7 +2,7 @@
  * Filename: controller.ts
  * Author: Nathan Filipowitz
  * Date: 2026-03-13
- * Purpose: Controller for the entire app
+ * Purpose: Controller for the shop
  */
 
 import * as model from "../models/db.ts";
