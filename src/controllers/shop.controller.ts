b75ae9fccadd16874dfc2f3da3bea6c6a7951f10
NathanFilipowitz <py58wpb@eduvaud.ch>
2026-03-16 10:52:29 +0100

    (feat) only use useful methods, to use in login page

--- src/controllers/shop.controller.ts
+++ src/controllers/shop.controller.ts
@@ -8,23 +8,12 @@
 import * as model from "../models/db.ts";
 
 const ProductController = {
-  create: (name: string, price: number, stock: number) => {
-    if (price < 0) throw new Error("Price cannot be negative");
-    if (stock < 0) throw new Error("Stock cannot be negative");
-    return model.createProduct(name, price, stock);
-  },
-
   getAll: () => model.getAllProducts(),
 
   getById: (id: number) => {
     const product = model.getProductById(id);
     if (!product) throw new Error("Product not found");
     return product;
-  },
-
-  updateStock: (id: number, newStock: number) => {
-    if (newStock < 0) throw new Error("Stock cannot be negative");
-    return model.updateProductStock(id, newStock);
   },
 
   remove: (id: number) => model.deleteProduct(id)
