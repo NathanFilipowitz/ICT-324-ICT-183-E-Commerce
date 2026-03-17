ecf70b14a0bf9bf305994170c7d880e156ed11e7
NathanFilipowitz <py58wpb@eduvaud.ch>
2026-03-17 13:41:39 +0100

    (feat) added basic shop controller

--- src/controllers/shop.controller.js
+++ src/controllers/shop.controller.js
@@ -1,14 +1,32 @@
-import {CatalogModel, ProductModel} from '../models/shop.model.js'
+/*
+ * Filename: controller.ts
+ * Author: Nathan Filipowitz
+ * Date: 2026-03-13
+ * Purpose: Controller for the shop
+ */
 
-export const CatalogController = {
+// import * as model from "../models/db.ts";
 
+const model = {
+  getAllProducts: () => {
+    return 0;
+  },
+  getProductById: (id) => {
+    return id;
+  },
+  deleteProduct: (id) => {
+    return id;
+  }
 }
 
-export const ProductController = {
-    getAllProducts: async () => {
-        return await ProductModel.getAllProducts();
-    },
-    getProductById: async (id) => {
-        return await ProductModel.getProductById(id);
-    }
-}
+const ProductController = {
+  getAll: () => model.getAllProducts(),
+
+  getById: (id) => {
+    const product = model.getProductById(id);
+    if (!product) throw new Error("Product not found");
+    return product;
+  },
+
+  remove: (id) => model.deleteProduct(id)
+};
